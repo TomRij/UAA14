e85ed036856439bd1d3febbf7f5a28a3e04f4497
Rijckaert Tom <200082@CCM-Asty-Moulin.local>
2023-09-05 14:30:44 +0200

    intro oo

---
+++ TomRijckaertExoChien/TomRijckaertExoChien/Chien.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomRijckaertExoChien
+{
+    class Chien
+    {
+        private string _nom;
+        private string _race;
+        private uint _age;
+        private bool _vacciner;
+        private bool _puce;
+        private string _genre;
+        private bool _sterilise;
+        private bool _dresse;
+
+        public Chien(string nom, string race, uint age, bool vacciner, bool puce, string genre, bool sterilise, bool dresse)
+        {
+            _nom = nom;
+            _race = race;
+            _age = age;
+            _vacciner = vacciner;
+            _puce = puce;
+            _genre = genre;
+            _sterilise = sterilise;
+            _dresse = dresse;
+        }
+
+        // Une méthode pour formater les attributs d'un chien dans une string 
+        public string AfficherCaractéristiques()
+        {
+            string chaine = "Nom : " + _nom + "Race : " + _race + " - Age : " + _age + "Vacciner : " + _puce + "Genre : " + "Sterilise : " + _sterilise + "Dresse " + _dresse;
+
+            return chaine;
+        }
+
+    }
+}
---
+++ TomRijckaertExoChien/TomRijckaertExoChien/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TomRijckaertExoChien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("première méthode");
+            Chien chien1 = new Chien("Victor", "chiouaoua", 18, true, )
+        }
+    }
+}
