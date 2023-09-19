76cd314d471c1a9b2e77e1c35aae57c0109e750d
Rijckaert Tom <200082@CCM-Asty-Moulin.local>
2023-09-19 14:31:01 +0200

    finir exo 1 et uml 2

---
+++ ACT_2_POO_exercices_simples/ACT_2_POO_exercices_simples/ACT_2_POO_exercices_simples/Cercle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACT_2_POO_exercices_simples
+{
+    class Cercle
+    {
+        private double _rayon;
+
+        public Cercle(double rayon)
+        {
+            _rayon = rayon;
+        }
+
+        public double CalculAire()
+        {
+
+        } 
+    }
+}
---
+++ ACT_2_POO_exercices_simples/ACT_2_POO_exercices_simples/ACT_2_POO_exercices_simples/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ACT_2_POO_exercices_simples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
