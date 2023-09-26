6a0b4ebe7ea2158fd5f77ae549c616b61639a36b
Rijckaert Tom <200082@CCM-Asty-Moulin.local>
2023-09-26 14:22:19 +0200

    interro 2 partie csharp

---
+++ I2P623_RijckaertTom/I2P623_RijckaertTom/FeuDeSignalisation.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I2P623_RijckaertTom
+{
+    class FeuDeSignalisation
+    {
+        private int _couleur;
+        private string _identifiant;
+
+        public FeuDeSignalisation(int couleur, string identifiant)
+        {
+            _couleur = couleur;
+            _identifiant = identifiant;
+        }
+
+        int Change(int couleur, string identifiant)
+        {
+            string etat_couleur = "";
+            for (int i = 0; i < 5; i++)
+            {
+                if (couleur == 0)
+                {
+                    return couleur = 1;
+
+                }
+                else if (couleur == 1)
+                {
+
+                    return couleur = 0;
+                }
+            }
+        }
+        public int GetCouleur()
+        {
+        }
+
+        public void Clignote(ref int couleur, string identifiant);
+        {
+            string etat_cligno = "";
+                for (int i = 0; i< 5; i++)
+                {
+                    if (_couleur == 0)
+                    {
+                        _couleur = 1;
+                        return etat_cligno = "allumé";
+                    }
+                    else
+                    {
+                        _couleur = 0;
+                        return etat_cligno = "étaint";
+                    }
+                }
+
+        }
+
+        string AfficheEtat(etat_couleur)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+
+            }
+        }
+
+
+    }
+    
+}
---
+++ I2P623_RijckaertTom/I2P623_RijckaertTom/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace I2P623_RijckaertTom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Feu Vert");
+
+            
+
+
+
+
+        }
+    }
+}
