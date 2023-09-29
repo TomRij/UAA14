b3f41ad07bdb1b1ac27c520aa6e6517a2827a00c
Rijckaert Tom <200082@CCM-Asty-Moulin.local>
2023-09-29 14:29:39 +0200

    commencement de Matching game

---
+++ MatchingGame_RijckaertTom/MatchingGame_RijckaertTom/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchingGame_RijckaertTom
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+
+
+
+        }
+
+        private void SetUpGame()
+            {
+                List<string> animalEmoji = new List<string>()
+                    {
+                        "🐈","🐈",
+                        "🐷","🐷",
+                        "🐐","🐐",
+                        "🦊","🦊",
+                        "🐴","🐴",
+                        "🦨","🦨",
+                        "🦉","🦉",
+                        "🐀","🐀",
+                    };
+
+            int index;
+            string nextEmoji;
+            Random alea = new Random();
+                foreach (TextBlock textBlock in grdMain.Children.OfType<TextBlock>())
+                {
+                    index = alea.Next(animalEmoji.Count); // index est de type int
+                                                            // nbalea est un objet de type Random()
+                    nextEmoji = animalEmoji[index]; // nextEmoji est de type string
+                    textBlock.Text = nextEmoji;
+                    animalEmoji.RemoveAt(index); // on retire un animal de la liste pour ne pas l’attribuer à nouveau.
+                }
+            }
+
+    }
+}
