8c998835132c28c414f72b4f945255fb91eeb202
Rijckaert Tom <200082@CCM-Asty-Moulin.local>
2023-10-03 13:58:04 +0200

    fin de l'exercice sur le cercle

--- ACT_2_POO_exercices_simples/ACT_2_POO_exercices_simples/ACT_2_POO_exercices_simples/Cercle.cs
+++ ACT_2_POO_exercices_simples/ACT_2_POO_exercices_simples/ACT_2_POO_exercices_simples/Cercle.cs
@@ -8,16 +8,34 @@ namespace ACT_2_POO_exercices_simples
 {
     class Cercle
     {
-        private double _rayon;
+        private float _rayon;
 
-        public Cercle(double rayon)
+        public Cercle(float rayon)
         {
             _rayon = rayon;
         }
 
         public double CalculAire()
         {
+            return Math.PI * _rayon * _rayon;
+        }
+        public double CalculePerimetre()
+        {
+            return 2 * Math.PI * _rayon;
+        }
 
-        } 
+        public void DiviserParDeux()
+        {
+            _rayon /= 2;
+        }
+
+        public void AfficherInfo()
+        {
+            Console.WriteLine("Caractéristiques du cercle :");
+            Console.WriteLine($"Rayon : {_rayon}");
+            Console.WriteLine($"Aire : {CalculAire()}");
+            Console.WriteLine($"Périmètre : {CalculePerimetre()}");
+
+        }
     }
 }
--- ACT_2_POO_exercices_simples/ACT_2_POO_exercices_simples/ACT_2_POO_exercices_simples/Program.cs
+++ ACT_2_POO_exercices_simples/ACT_2_POO_exercices_simples/ACT_2_POO_exercices_simples/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace ACT_2_POO_exercices_simples
+﻿using System;
+
+namespace ACT_2_POO_exercices_simples
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Veuillez entrer le rayon du cercle :");
+
+            if (float.TryParse(Console.ReadLine(), out float rayon))
+            {
+                Cercle cercle = new Cercle(rayon);
+                cercle.AfficherInfo();
+
+                Console.WriteLine("\nLe cercle diviser par deux :");
+                cercle.DiviserParDeux();
+                cercle.AfficherInfo();
+            }
+            else
+            {
+                Console.WriteLine("Entrée invalide. Veuillez entrer un nombre valide pour le rayon.");
+            }
         }
     }
 }
