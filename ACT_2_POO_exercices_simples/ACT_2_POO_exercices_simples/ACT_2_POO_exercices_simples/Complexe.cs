50d612cbccd9d94fa433cb38446986e9938892fb
Rijckaert Tom <200082@CCM-Asty-Moulin.local>
2023-10-20 14:21:35 +0200

    commencement nombre complexe et fin des 4 UML

---
+++ ACT_2_POO_exercices_simples/ACT_2_POO_exercices_simples/ACT_2_POO_exercices_simples/Complexe.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACT_2_POO_exercices_simples
+{
+    class Complexe
+    {
+        private int _reelle;
+        private int _imaginaire;
+
+        public Complexe(int reelle, int imaginaire)
+        {
+            _reelle = reelle;
+            _imaginaire = imaginaire;
+        }
+
+        public string AfficheComplexe()
+        {
+            return $"({_reelle}, {_imaginaire}";
+        }
+
+        public int CalculeModule()
+        {
+            return _reelle * _reelle + _imaginaire * _imaginaire;
+        }
+
+        public void Ajoute(Complexe x)
+        {
+
+        }
+    }
+}
