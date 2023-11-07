23932e6b53d6f49a77c63ce111d8aa3a67db6782
Rijckaert Tom <200082@CCM-Asty-Moulin.local>
2023-11-07 14:29:04 +0100

    Commencement exercice Elephant

---
+++ Tom_Rijckaert_ACT_3_Éléphants/Tom_Rijckaert_ACT_3_Éléphants/Elephant.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tom_Rijckaert_ACT_3_Éléphants
+{
+    class Elephant
+    {
+        private string _nom;
+        private uint _TailleOreilles;
+
+        public Elephant(string nom, uint tailleOreilles)
+        {
+            _nom = nom;
+            _TailleOreilles = tailleOreilles;
+        }
+        public string Nom
+        {
+            get { return _nom; }
+        }
+        public uint TailleOreilles
+        {
+            get { return _TailleOreilles; }
+        }
+        public string Affichequijesuis()
+        {
+            return _nom + " a une taille d'oreille de " + _TailleOreilles;
+        }
+
+        public void EcouteMessage(string message, Elephant quiDit)
+        {
+            Console.WriteLine(_nom + " a entendu un message \n" + quiDit._nom + " a dit : " + message);
+
+        }
+        public void EnvoieMessage(string message, Elephant quiRecoit)
+        {
+            quiRecoit.EcouteMessage(message, this);
+        }
+
+
+    }
+}
---
+++ Tom_Rijckaert_ACT_3_Éléphants/Tom_Rijckaert_ACT_3_Éléphants/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Tom_Rijckaert_ACT_3_Éléphants
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenue dans cet échange d'éléphants");
+            Console.WriteLine("Tapez : ");
+            Console.WriteLine("1. Pour afficher les informations de Zazou,");
+            Console.WriteLine("2. Pour Titi");
+            Console.WriteLine("3. Pour les échanger");
+            Console.WriteLine("4. ");
+        }
+    }
+}
