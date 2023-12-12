d9cf58fcef1e2067285020b79753465eddfddc43
Rijckaert Tom <200082@CCM-Asty-Moulin.local>
2023-12-12 15:10:21 +0100

    Examen Poo du 12 décembre et il fonctionne et oui incroyable

---
+++ CeUAA14Partie2_dec23_RijckaertTom/CeUAA14Partie2_dec23_RijckaertTom/PaintBallGun.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CeUAA14Partie2_dec23_RijckaertTom
+{
+    class PaintBallGun
+    {
+        private int _nbreBalles;
+        private int _chargeur;
+
+        public PaintBallGun(int nbreBalles, int chargeur)
+        {
+            _chargeur = chargeur;
+            _nbreBalles = nbreBalles;
+        }
+
+        public int nbreBalles
+        {
+            get { return _nbreBalles; }
+            set { _nbreBalles = value; }
+        }
+        public int chargeur
+        {
+            get { return _chargeur; }
+            set { _chargeur = value; }
+        }
+        // quand le joueur tir cela fait en sorte qu'il perd une balle dans son chargeur
+        public int Tirer()
+        {
+            return _chargeur--;
+        }
+        // cela permet au joueur de recharger les balles de en poches sont transmises au chargeur
+        public int Recharger()
+        {
+            int a = 0;
+            _chargeur = a;
+            _nbreBalles -= a;
+            _nbreBalles -= 16;
+            _chargeur += 16;
+
+            return _chargeur & _nbreBalles;
+        }
+        // cela indique l'état des munitions dans la poche du joueur (combien il en reste)
+        public string Vide()
+        {
+            if (_nbreBalles == 0)
+            {
+                return "Vous n'avez plus de munitions !";
+            }
+            if(_nbreBalles < 0){
+                return "Vous êtes endètées WARNING !";
+            }
+            else
+            {
+                return "Il vous reste des munitions dans votre poche";
+            }
+        }
+        // cela permet de rajouter des balles dans la poche du joueur
+        public int PrendreMunitions()
+        {
+            return _nbreBalles += 30;
+        }
+        // cela permet d'indiquer l'état du chargeur de joueur
+        public string ChargeurRecharger()
+            {
+            if (_chargeur == 0)
+            {
+                return "Votre arme n'a plus de munitions ! (il faut recharger)";
+            }
+            if (_chargeur < 0)
+            {
+                return "Vous ne pouvez pas tirer à blanc attention votre arme va finir par casser si vous continué !";
+            }
+            else                
+            {
+                    return "Votre chargeur n'est pas vide";
+                }
+            }
+
+            
+        }
+    }
+
---
+++ CeUAA14Partie2_dec23_RijckaertTom/CeUAA14Partie2_dec23_RijckaertTom/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace CeUAA14Partie2_dec23_RijckaertTom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tapper = "";
+            PaintBallGun joueur1 = new PaintBallGun(30, 0);
+            while (true)
+            {
+                // Présentation du jeu
+                Console.WriteLine("Bienvenue dans ce jeu de tir --- Vous commencez avec 30 balles");
+                Console.WriteLine("==============================================================");
+                // Affiche au joueur le nombre de balles restant dans sa poche et dans son chargeur
+                Console.WriteLine("Vous avez un totale de " + joueur1.nbreBalles + " balles dans votre poches et " + joueur1.chargeur + " balles dans votre chargeur");
+                Console.WriteLine(joueur1.ChargeurRecharger());
+                Console.WriteLine(joueur1.Vide());
+
+                Console.WriteLine("");
+                Console.WriteLine("Espace pour tirer");
+                Console.WriteLine("r pour recharger");
+                Console.WriteLine("+ pour reprendre des munitions");
+                Console.WriteLine("q pour quitter");
+
+                tapper = Console.ReadLine();
+                // fait exécuter la fonction de tir
+                if (tapper == " ")
+                {
+                    joueur1.Tirer();
+                    Console.WriteLine("=> Tir effectué!");
+                    
+                }
+                // fait exécuter la fonction de recharger
+                if (tapper == "r") 
+                {  
+                    joueur1.Recharger();
+                }
+                // fait exécuter la fonction de faire le plein dans ses poches
+                if (tapper == "+")
+                {
+                    joueur1.PrendreMunitions();
+                }
+                // fait sortir le joueur du jeu
+                if (tapper == "q")
+                {
+                    break;
+                }
+            }
+            
+            
+        }
+    }
+}
+
