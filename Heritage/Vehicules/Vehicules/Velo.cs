83f26faf569ffe612c289660ab24ee51b1286c6e
Rijckaert Tom <200082@CCM-Asty-Moulin.local>
2024-01-09 14:31:08 +0100

    Presque fini exo 1

---
+++ Heritage/Vehicules/Vehicules/Velo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicules
+{
+    class Velo:Vehicule
+    {
+        protected string _type;
+        protected bool _estElectrique;
+
+        public string Type
+        {
+            get { return _type; }
+        }
+        public bool EstElectrique
+        {
+            get { return _estElectrique; }
+        }
+
+        public Velo(string type, bool electrique, string modele, string marque, string couleur, decimal prix) : base (modele, marque, couleur, prix)
+        {
+            _type = type;
+            _estElectrique = electrique;
+        }
+    }
+}
---
+++ Heritage/Vehicules/Vehicules/Voiture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicules
+{
+    class Voiture:Vehicule
+    {
+        protected string _motorisation;
+        protected bool _gps;
+
+        public string Motorisation { get { return _motorisation; } }
+        public bool Gps { get { return _gps; } }
+
+        public Voiture(string motorisation, bool gps, string modele, string marque, string couleur, decimal prix) : base(modele, marque, couleur, prix)
+        {
+            _motorisation = motorisation;
+            _gps = gps;
+        }
+        public override string chaineVoiture = "";
+
+    }
+}
