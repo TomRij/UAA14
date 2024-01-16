560e5175e7f92da1c0d457fa93cfacca0dae4288
Rijckaert Tom <200082@CCM-Asty-Moulin.local>
2024-01-16 14:33:14 +0100

    Fin Animal, Lapin et commencement du chat et à faire chien

---
+++ Heritage/HeritageExo2/HeritageExo2/Animal.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeritageExo2
+{
+    class Animal
+    {
+        protected string _nom;
+        protected string _dateDeNaissance;
+        protected decimal _numeroPuce;
+        protected bool _concours;
+
+        public string Nom
+        {
+            get
+            {
+                return _nom;
+            }
+            set 
+            {
+                _nom = value;
+            }
+        }
+        public string DateDeNaissance
+        {
+            get
+            {
+                return _dateDeNaissance;
+            }
+        }
+        public decimal NumeroPuce
+        {
+            get
+            {
+                return _numeroPuce;
+            }
+        }
+        public bool Concours
+        {
+            get
+            {
+                return _concours;
+            }
+            set
+            {
+                _concours = value;
+            }
+        }
+
+        public Animal(string nom, string dateDeNaissance, decimal numeroPuce, bool concours)
+        {
+            _nom = nom;
+            _dateDeNaissance = dateDeNaissance;
+            _numeroPuce = numeroPuce;
+            _concours = concours;
+        }
+
+        public virtual string Affiche()
+        {
+            return "L'hêtre " + Nom + " a pour est né le : " + DateDeNaissance + ", son numéeo de puce est le " + NumeroPuce + " et participe " + Concours+".";
+        }
+
+    }
+}
---
+++ Heritage/HeritageExo2/HeritageExo2/Chat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeritageExo2
+{
+    class Chat : Animal
+    {
+        public decimal TailleOreille
+        {
+            get { return _tailleOreille; }
+        }
+        bool Sauter(bool type)
+        {
+            return type;
+        }
+
+        public Lapin(decimal tailleOreille, string nom, string dateDeNaissance, decimal numeroPuce, bool concours) : base(nom, dateDeNaissance, numeroPuce, concours)
+        {
+            _tailleOreille = tailleOreille;
+        }
+
+        public override string Affiche()
+        {
+            return "Le Lapin " + Nom + " a pour date de naissance : " + DateDeNaissance + ", comme numéeo de puce " + NumeroPuce + ", participe au concours : " + Concours + ", à comme taille d'oreille : " + TailleOreille + ", saute : " + saut + ".";
+        }
+    }
+}
