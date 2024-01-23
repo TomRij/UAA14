2e4e2abae925d28096edafe4270fb7bab77c22a0
Rijckaert Tom <200082@CCM-Asty-Moulin.local>
2024-01-23 14:31:02 +0100

    commencement BDD csharp

---
+++ CRUDsurBDDimmo/CRUDsurBDDimmo/Methode.cs
@@ -0,0 +1,29 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDsurBDDimmo
+{
+    public struct Methode
+    {
+        public string DefinirChemainBDD()
+        {
+            return "server=localhost; database=immo; port=3306;User Id=root; password=root";
+        }
+        public bool ChercheUtilisateurs(out DataSet infos)
+        {
+            bool ok = false;
+            MySqlConnection maConnection = new MySqlConnection(DefinirCheminBDO);
+            string query = "";
+            try
+            {
+
+            }
+        }
+
+    }
+}
---
+++ CRUDsurBDDimmo/CRUDsurBDDimmo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Data;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+using Google.Protobuf.WellKnownTypes;
+
+namespace CRUDsurBDDimmo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] donneers = new string[5];
+            DataSet immoUsers;
+            MethodesDuModule MonModule = new MethodesDuModule;
+            Console.WriteLine("Liste des utilisateurs");
+            Console.WriteLine("----------------------");
+            if (MonModule.ChercheUtilisateurs(out immoUsers))
+            {
+                string listeUser = MonModule.AfficheDataUsers(immoUsers);
+                Console.WriteLine(listeUser);
+            }
+            else
+            {
+                Console.WriteLine("Pb lors de la lecture des données");
+            }
+            Console.WriteLine("Ajout d'un utilisateur :");
+            Console.WriteLine("------------------------");
+            Console.WriteLine("Nom ? ");
+            donneers[0] = Console.ReadLine();
+            Console.WriteLine("Prénom ? ");
+            donneers[1] = Console.ReadLine();
+            Console.WriteLine("Login ? ");
+            donneers[2] = Console.ReadLine();
+            Console.WriteLine("Mot de Passe ? ");
+            donneers[3] = Console.ReadLine();
+            Console.WriteLine("Rôle ? ");
+            donneers[4] = Console.ReadLine();
+
+            if (MonModule.AjouterUser(donneers))
+            {
+                Console.WriteLine("Utilisateur ajouté :");
+            }
+            else
+            {
+                Console.WriteLine("Problème lors de l'ajout !");
+            }
+            Console.ReadLine();
+
+            static string DefinirCheminBD() // détermine la chaîne de connexion
+            {
+                return "server=10.10.51.98;database=immo;port=3306;User Id=root;password=root";
+            }
+        }
+    }
+}
