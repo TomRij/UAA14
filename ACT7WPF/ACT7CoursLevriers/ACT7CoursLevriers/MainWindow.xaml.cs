f1daa6bafc30d9f4ca92efbb30f38c62f1f38aad
Rijckaert Tom <200082@CCM-Asty-Moulin.local>
2024-01-26 14:27:59 +0100

    Commencement ACT7

---
+++ ACT7WPF/ACT7CoursLevriers/ACT7CoursLevriers/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ACT7CoursLevriers
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+           
+
+            // Pari
+            int _pari;
+
+            // Parieur
+            int _portefeuille = 0;
+            string _nom = "";
+            //_pari = pari;
+
+            // Chien
+            string _nomChien = "";
+            int numeroChien = 0;
+            int _position = 0;
+
+        }
+    }
+}
